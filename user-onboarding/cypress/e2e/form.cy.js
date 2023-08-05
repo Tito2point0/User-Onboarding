d5d87e8b006504de0dcab6f6db67a3f1f924a9de
sanjiro <defconx00@gmail.com>
2023-08-05 03:43:04 -0400

    cypress final version

--- user-onboarding/cypress/e2e/form.cy.js
+++ user-onboarding/cypress/e2e/form.cy.js
@@ -1,61 +1,73 @@
-describe('settings page', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000')
-    })
+describe("settings page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
 
+  //my list of helpers
+  const textInput = () => cy.get("input[name=UserName]");
+  const electronicMailInput = () => cy.get("input[name=email]");
+  const dontLook = () => cy.get("input[name=password]");
+  const boxinput = () => cy.get("input[name=tos]");
+  const submitButton = () => cy.get("input[type=submit]");
 
-    //my list of helpers
-    const textInput = () => cy.get('input[name=UserName]');
-    const electronicMailInput = () => cy.get('input[name=email]');
-    const dontLook = () => cy.get('input[name=password]')
-     const boxinput = () => cy.get('input[name=tos]')
+  it("grabbing name from dom", () => {
+    textInput()
+      .should("have.value", "")
+      .type("Jorge george")
+      .should("have.value", "Jorge george");
+  });
 
+  it("testing email", () => {
+    electronicMailInput()
+      .should("have.value", "")
+      .type("gaga@gmail.com")
+      .should("have.value", "gaga@gmail.com");
+  });
 
+  it("setting password", () => {
+    dontLook()
+      .should("have.value", "")
+      .type("jasonjason")
+      .should("have.value", "jasonjason");
+  });
 
+  it("checking box", () => {
+    boxinput().click();
+  });
 
-    //element check 
-    it("elements are changed", () => {
-        textInput().should("exist");
-        electronicMailInput().should("exist")
-    })
+  it("full send", () => {
+    textInput().should("have.value", "").type("Jorge george");
+    electronicMailInput().should("have.value", "").type("gaga@gmail.com");
+    dontLook()
+      .should("have.value", "")
+      .type("jasonjason")
+      .should("have.value", "jasonjason")
+    boxinput().click()
+      submitButton()
+          .click()
+      
     
-
-
-    //so this is 
-
-    it("grabbing name from dom", () => {
+  });
+    
+    it("testing form validation", () => {
         textInput()
-            .should("have.value", "")
             .type("Jorge george")
-            .should("have.value", "Jorge george");
-
-    })
-
- 
- 
-    it("testing email", () => {
+            .clear()
         electronicMailInput()
             .should("have.value", "")
-            .type("gaga@gmail.com")
-            .should("have.value", "gaga@gmail.com");
-
-    })
-
-    it("setting password", () => {
+            .type("gag2@gmail.com")
+            .clear()
         dontLook()
             .should("have.value", "")
             .type("jasonjason")
-            .should("have.value", "jasonjason");
-
-    })
-
-    it("checking box", () => {
+            .clear()
         boxinput()
-        .click()
+            .click()
+            .uncheck()
+
 
     })
 
 
  
-
-})
+});
